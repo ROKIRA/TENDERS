b0056478517bf3a6694dc15eb82d085f6e9c5a51
Tsarev Kirill <gufigufi.42@gmail.com>
2014-09-25 02:09:02 +0300

    header first

--- app/views/verstka.php
+++ app/views/verstka.php
@@ -12,14 +12,59 @@
     <script src="js/bootstrap.min.js"></script>
 
     <script src="js/main_script.js"></script>
+    <script src="js/jquery-1.11.1.min.js"></script>
 </head>
 <body>
 
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6 .col-md-offset-3">
-                header
+            <div class="col-lg-12 header">
+                <div class="col-lg-1">
+                    <a href="#"><img src="images/logo.jpg" alt="Логотип"/></a>
+                </div>
+
+                    <div class="col-lg-11">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1>Центре развития регионов при Харьковском национальном уневерситете городского хозяйства имени О.М</h1>
+                            </div>
+                            <div class="col-lg-4">
+                                <address>Тел: +380-57-707-31-52 Тел: +380 50 50 555</address>
+                            </div>
+                            <div class="col-lg-2">
+                                <ul>
+                                    <li class="lang"><a class="btn" href="#">рус</a></li>
+                                    <li class="lang"><a class="btn" href="#">eng</a></li>
+                                    <li class="lang"><a class="btn" href="#">укр</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="navbar">
+                            <div class="navbar-inner">
+                                <div class="container">
+
+                                    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+                                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </a>
+
+                                    <!-- Be sure to leave the brand out there if you want it shown -->
+                                    <a class="brand" href="#">Project name</a>
+
+                                    <!-- Everything you want hidden at 940px or less, place within here -->
+                                    <div class="nav-collapse">
+                                        <!-- .nav, .navbar-search, .navbar-form, etc -->
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                </div>
+
             </div>
         </div>
     </div>
