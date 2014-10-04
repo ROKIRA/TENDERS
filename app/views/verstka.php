9027d36d1e98fb0b6a08743304c1606efa7ffba0
Tsarev Kirill <gufigufi.42@gmail.com>
2014-10-04 03:32:19 +0300

    news

--- app/views/verstka.php
+++ app/views/verstka.php
@@ -27,10 +27,10 @@
                     <div class="col-lg-11">
                         <div class="row">
                             <div class="col-lg-6">
-                                <h1>Центре развития регионов при Харьковском национальном уневерситете городского хозяйства имени О.М</h1>
+                                <h1><strong>Центре развития регионов</strong> Харьковском национальном университете городского хозяйства имени О.М Бекетова</h1>
                             </div>
                             <div class="col-lg-4">
-                                <address>Тел: +380-57-707-31-52 Тел: +380 50 50 555</address>
+                                <address>Тел: +380-57-707-31-52 e-mail: region.dc@ukr.net</address>
                             </div>
                             <div class="col-lg-3 sprite-languages">
                                 <ul>
@@ -41,22 +41,6 @@
                             </div>
                         </div><!-- .row -->
 
-                        <div class="row test">
-                            <div class="col-lg-3"></div>
-                            <div class="col-lg-6">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis urna nec molestie maximus. Maecenas rutrum ex vitae convallis luctus. In non egestas nulla. Nam ut elit eu velit imperdiet tempus at a ligula. Donec rhoncus, metus at feugiat gravida, est odio euismod neque, eget ultricies mauris ligula elementum leo. Curabitur condimentum pulvinar turpis, non cursus nunc dictum id. Aenean eu luctus augue. Nam et eros id risus porttitor pellentesque molestie et est. Proin varius dignissim eros, sit amet condimentum mi cursus in. Vestibulum laoreet mollis risus, et commodo quam molestie at. Ut nibh ligula, dictum ac fringilla sit amet, cursus et enim. Donec aliquam elit ut libero luctus, eget aliquam erat lobortis.</p>
-
-                                <p>Suspendisse leo ante, sollicitudin eget fringilla nec, rhoncus ac orci. Vivamus auctor dolor vitae augue faucibus lobortis. Proin et purus ante. Pellentesque blandit interdum justo porta tempus. Nunc in ligula varius, eleifend diam id, sodales mauris. Vivamus ut lectus nec magna venenatis ultricies. Morbi eu lectus vel ligula sagittis dictum. Etiam eleifend facilisis ex, a semper nibh vehicula sed. Cras sit amet egestas urna, non euismod urna. Sed pretium ante quis fringilla maximus.</p>
-
-                                <p>Nam sem nunc, laoreet in risus ac, placerat congue ipsum. Donec non pellentesque tortor. Donec sagittis varius orci, quis commodo sapien fermentum sed. Aliquam lacinia sem tristique massa gravida, a dapibus felis facilisis. Vestibulum hendrerit pellentesque mauris, et rhoncus quam mollis nec. Maecenas nisi est, placerat sed maximus sed, tempus non erat. Fusce ut est in sem volutpat finibus. Nulla facilisi. Morbi cursus risus est, quis venenatis quam tincidunt nec. Mauris lobortis purus ut dui iaculis egestas. In ut dui rutrum, sodales neque quis, venenatis quam. Proin non velit ut risus dictum congue. Integer eget elit fringilla, placerat felis sit amet, accumsan lorem. Ut efficitur consectetur augue, vel malesuada turpis. Pellentesque scelerisque lorem felis, ac tincidunt lorem pulvinar quis.</p>
-
-                                <p>Aliquam dignissim ut nisi sit amet consequat. Nunc rhoncus est erat, a ultrices diam hendrerit volutpat. Maecenas at convallis odio, eget ultricies ligula. Vivamus luctus nulla ipsum, vel sagittis erat commodo eu. Curabitur ullamcorper tempor erat at tincidunt. Phasellus malesuada laoreet lacus quis pulvinar. Donec convallis libero sed magna dignissim euismod. Nunc risus dolor, varius at commodo id, mollis quis mi. Curabitur sagittis et mi nec ultricies. Mauris ac porta enim, ut condimentum felis. Duis nibh erat, efficitur quis est sed, lobortis iaculis risus. Nam aliquet arcu convallis tristique fermentum. Praesent ac pulvinar mauris, in aliquam enim. Phasellus quis ornare nibh.</p>
-
-                                <p>Praesent suscipit, nunc ac tempor varius, mi eros blandit tellus, ut tristique odio nunc vitae ligula. Vestibulum hendrerit hendrerit dignissim. Donec egestas velit ut arcu dignissim, vitae ornare justo fermentum. Ut vitae varius mauris. Morbi cursus viverra nulla, vel mollis enim venenatis a. Donec sit amet aliquet diam. Nulla et facilisis magna, vitae malesuada sem. Maecenas id dapibus erat.</p>
-
-                            </div>
-                            <div class="col-lg-3"></div>
-                        </div>
 
                         <ul class="nav nav-pills">
                             <li class="active"><a href="#">Главная</a></li>
@@ -71,12 +55,25 @@
             </div>
         </div><!--.row-->
 
-        <div class="row test">
+        <div class="row center">
             <div class="col-lg-3"></div>
             <div class="col-lg-6">
-
             </div>
-            <div class="col-lg-3"></div>
+            <div class="col-lg-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Новости</h3></div>
+                    <div class="panel-body">
+                        <ul>
+                            <li><a href="#">Новость1</a></li>
+                            <li><a href="#">Новость2</a></li>
+                            <li><a href="#">Новость3</a></li>
+                            <li><a href="#">Новость4</a></li>
+                            <li><a href="#">Новость5</a></li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Все новости</a>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
